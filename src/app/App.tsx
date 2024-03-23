a16363c3f2ed98bd597f7fb63329ae26c07dd0ba
Margo <m.yunanova@gmail.com>
2024-03-23 13:48:03 +0300

    feat: implement Button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
 import { Accordion } from '../ui/accordion';
+import { Button } from '../ui/button';
 import './App.css';
 function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
       <Accordion />
+      <Button variant="contained">кнопка</Button>
+      <Button variant="outlined">кнопка</Button>
     </div>
   );
 }
---
+++ src/ui/button/Button.ts
@@ -0,0 +1,24 @@
+import { Button as ButtonBase, styled, ButtonProps } from '@mui/material';
+
+export const Button = styled(ButtonBase, {
+  overridesResolver: (props, styles) => [
+    styles.root,
+    props.variant === 'contained' && styles.contained,
+    props.variant === 'outlined' && styles.outlined,
+  ],
+})<ButtonProps>(({ theme }) => ({
+  width: '100%',
+  textTransform: 'capitalize',
+  borderRadius: '8px',
+  paddingTop: '8px',
+  paddingBottom: '8px',
+  fontSize: '16px',
+  letterSpacing: '0.15px',
+  '&:active': {
+    backgroundColor: theme.palette.background.brandDayDark,
+    color: theme.palette.text.baseWhite,
+  },
+  '&.Mui-disabled': {
+    opacity: '0.6',
+  },
+}));
