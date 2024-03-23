8928cfd09fa636297c3c26323581557b3903281c
Margo <m.yunanova@gmail.com>
2024-03-23 13:47:11 +0300

    feat: implement Accordion

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Accordion } from '../ui/accordion';
+import './App.css';
+function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/accordion/Accordion.tsx
@@ -0,0 +1,62 @@
+import {
+  Accordion as AccordionBase,
+  styled,
+  AccordionProps as AccordionPropsBase,
+  AccordionSummary,
+  AccordionSummaryProps as AccordionSummaryPropsBase,
+  AccordionDetails,
+  AccordionDetailsProps as AccordionDetailsPropsBase,
+  Typography,
+} from '@mui/material';
+import { ChevronDown } from 'react-coolicons';
+
+import { FC } from 'react';
+
+const StyledAccordion = styled(AccordionBase)<AccordionPropsBase>(() => ({
+  padding: '24px 0px',
+  boxShadow: 'none',
+  borderBottom: 'solid 1px',
+  borderColor: `rgba(24, 20, 31, 0.15)`,
+}));
+
+const StyledAccordionSummary = styled(
+  AccordionSummary,
+)<AccordionSummaryPropsBase>(({ theme }) => ({
+  padding: '0px 0px',
+  '& .MuiAccordionSummary-expandIconWrapper': {
+    color: theme.palette.text.primary,
+  },
+}));
+
+const StyledAccordionDetails = styled(
+  AccordionDetails,
+)<AccordionDetailsPropsBase>(() => ({
+  padding: '0px 0px',
+}));
+
+interface AccordionProps {
+  title?: string;
+  details?: string;
+  id?: string;
+}
+
+export const Accordion: FC<AccordionProps> = ({
+  title = 'Как подключить подписку',
+  details = 'Подписку можно подключить в мобильном приложении или на сайте tinkoff.ru. Если у вас есть приложение Тинькофф, на главном экране перейдите в раздел «Кэшбэк и бонусы» → подписка Tinkoff Pro → «Подключить».',
+  id = '1',
+}) => {
+  return (
+    <StyledAccordion>
+      <StyledAccordionSummary
+        expandIcon={<ChevronDown />}
+        aria-controls={`panel${id}-content`}
+        id={`panel${id}-header`}
+      >
+        <Typography>{title}</Typography>
+      </StyledAccordionSummary>
+      <StyledAccordionDetails>
+        <Typography>{details}</Typography>
+      </StyledAccordionDetails>
+    </StyledAccordion>
+  );
+};
