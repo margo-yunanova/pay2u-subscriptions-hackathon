a758ff68b3757d2c3c039f14a4fdd86c3c95b38f
Margo <m.yunanova@gmail.com>
2024-03-24 23:27:21 +0300

    feat: implement CatalogCard

---
+++ src/widgets/catalog-card/CatalogCard.tsx
@@ -0,0 +1,58 @@
+import {
+  CardActionArea,
+  Card as CardBase,
+  CardProps,
+  Stack,
+  styled,
+} from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Chip } from '../../shared/ui/chip';
+
+export interface CatalogCardProps {
+  title?: string;
+  cashback?: string;
+  logo?: string;
+  sum?: string;
+}
+
+const StyledCatalogCard = styled(CardBase)<CardProps>(() => ({
+  borderRadius: '10px',
+}));
+
+export const CatalogCard: FC<CatalogCardProps> = ({
+  title,
+  cashback,
+  logo,
+  sum,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <StyledCatalogCard>
+      <CardActionArea onClick={() => navigate(`/catalog/${title}`)}>
+        <CardContent>
+          <Stack flexDirection="row" gap="12px" alignItems="center">
+            <CardMedia
+              component="img"
+              image={logo}
+              alt={`Логотип ${title}`}
+              sx={{
+                width: '44px',
+                height: '44px',
+              }}
+            />
+            <Stack flexDirection="column" flexGrow={1}>
+              <Typography variant="h3">{title}</Typography>
+              <Typography variant="body2">Подписка от {sum} ₽</Typography>
+            </Stack>
+            <Chip label={`${cashback}% кешбэк`} />
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </StyledCatalogCard>
+  );
+};
---
+++ src/widgets/catalog-card/index.ts
@@ -0,0 +1,1 @@
+export { CatalogCard } from './CatalogCard';
