1683ab7c19cd6faf1bf52fbfc191810f8b73937f
Margo <m.yunanova@gmail.com>
2024-03-24 20:16:55 +0300

    refactor: typography

--- src/widgets/my-subscriptions-card/MySubscriptionsCard.tsx
+++ src/widgets/my-subscriptions-card/MySubscriptionsCard.tsx
@@ -4,29 +4,31 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 import { FC } from 'react';
-import logo from '../../assets/ivi.png';
+import image from '../../assets/ivi.png';
 
-const StyledMySubscriptionsCard = styled(CardBase)<CardProps>(({ theme }) => ({
-  width: '144px',
+const StyledMySubscriptionsCard = styled(CardBase)<CardProps>(() => ({
+  width: '160px',
   borderRadius: '17px',
   position: 'relative',
 }));
 
-interface MySubscriptionsCardProps {
+export interface MySubscriptionsCardProps {
   title?: string;
   period?: string;
   dueDate?: string;
+  logo?: string;
 }
 
 export const MySubscriptionsCard: FC<MySubscriptionsCardProps> = ({
   title = 'IVI',
   period = '3',
-  dueDate = '11.06,2024',
+  dueDate = '11.06.2024',
+  logo = image,
 }) => {
   const theme = useTheme();
 
   return (
-    <StyledMySubscriptionsCard>
+    <StyledMySubscriptionsCard elevation={4}>
       <CardContent
         sx={{
           display: 'flex',
@@ -35,12 +37,16 @@ export const MySubscriptionsCard: FC<MySubscriptionsCardProps> = ({
           padding: '38px 8px 12px',
         }}
       >
-        <Typography>{title}</Typography>
-        <Typography sx={{ fontSize: '12px' }}>
+        <Typography variant="h3">{title}</Typography>
+        <Typography variant="body1" letterSpacing={0}>
           Подписка на {period} месяца
         </Typography>
         <Typography
-          sx={{ fontSize: '10px', color: theme.palette.text.greyDusk1 }}
+          sx={{
+            fontSize: '10px',
+            color: theme.palette.text.greyDusk1,
+            fontWeight: '300',
+          }}
         >
           Активна до: {dueDate}
         </Typography>
@@ -55,6 +61,7 @@ export const MySubscriptionsCard: FC<MySubscriptionsCardProps> = ({
           position: 'absolute',
           top: '-8px',
           right: '-8px',
+          zIndex: '-1',
         }}
       />
     </StyledMySubscriptionsCard>
