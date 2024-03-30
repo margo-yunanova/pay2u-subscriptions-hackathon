665d741b6713cacbe96de52be24f23be9367e524
Margo <m.yunanova@gmail.com>
2024-03-30 18:17:34 +0300

    fix: add scroll restoration for all pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,57 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+  createBrowserRouter,
+} from 'react-router-dom';
 import { CatalogPage } from '../pages/catalog-page';
 import { services } from '../pages/catalog-page/catalogMock';
 import { HomePage } from '../pages/home-page';
 import { popular, subscriptions } from '../pages/home-page/homeMock';
 import { MySubscriptionPage } from '../pages/my-subscription-page';
 import { SubscriptionCardPage } from '../pages/subscription-card-page';
+import { SubscriptionFormPage } from '../pages/subscription-form-page';
 import './App.css';
 
-function App() {
-  return (
-    <Routes>
-      <Route
-        path="/home"
-        element={
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <ScrollRestoration />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: 'home',
+        element: (
           <HomePage
             popularSubscriptions={popular}
             mySubscriptionsCard={subscriptions}
           />
-        }
-      />
-      <Route path="/catalog" element={<CatalogPage catalogCard={services} />} />
-      <Route
-        path="/catalog/:title"
-        element={<SubscriptionCardPage service={services[8]} />}
-      />
-      <Route path="/mysubscriptions" element={<MySubscriptionPage />} />
-    </Routes>
-  );
-}
+        ),
+      },
+      {
+        path: 'catalog',
+        element: <CatalogPage catalogCard={services} />,
+      },
+      {
+        path: 'catalog/:title',
+        element: <SubscriptionCardPage service={services[8]} />,
+      },
+      {
+        path: 'mysubscriptions',
+        element: <MySubscriptionPage />,
+      },
+      {
+        path: 'form',
+        element: <SubscriptionFormPage />,
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,12 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { theme } from './app/themes/theme';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
